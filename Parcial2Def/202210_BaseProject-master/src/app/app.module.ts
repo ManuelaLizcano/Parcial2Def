d9ed35217aab1de5aeca0fe514c8e93e24d2a1f4
Manuela Lizcano <m.lizcanoc@uniandes.edu.co>
2023-11-27 08:56:39 -0500

    commits

--- Parcial2Def/202210_BaseProject-master/src/app/app.module.ts
+++ Parcial2Def/202210_BaseProject-master/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+//cambiar nombre
+import { CambiarModule } from './cambiar/cambiar.module';
+//en caso de que existan relaciones 1 a 1 se debe importar
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    CambiarModule, 
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
